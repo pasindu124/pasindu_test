e15be932c9dafdc214f5f50035bc0eb71b54693d
Pasindu Weerasinghe <pasinduw@salpo.com>
2019-05-14 11:52:20 +0530

    SignIn component validations

--- components/SignIn.js
+++ components/SignIn.js
@@ -1,47 +1,80 @@
 import React from 'react';
 import { StyleSheet, Text, View, KeyboardAvoidingView, TouchableOpacity, TextInput } from 'react-native';
+import ValidationComponent from 'react-native-form-validator';
+export default class SignIn extends ValidationComponent {
+    constructor(props){
+        super(props)
+        this.state= {
+            email: '',
+            password: '',
+            errorEmail: '',
+            errorPassword: ''
+        }
+    }
+    onSubmit = () => {
+        this.validate({
+            email: { email: true},
+            password: {required: true}
+        });
+        if (this.getErrorsInField('email')) {
+            this.setState({errorEmail: this.getErrorsInField('email')[0]});
+        }
+        if (this.getErrorsInField('password')) {
+            this.setState({errorPassword: this.getErrorsInField('password')[0]});
+        }
+    }
 
-export default class SignIn extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>SignIn</Text>
-        <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
-            <View style={{ flexDirection: 'row'}} >
-                <Text style={styles.textInfo}>Email</Text>
-                <TextInput 
-                    placeholder="Email"
-                    returnKeyType="next"
-                    keyboardType='email-address'
-                    autoCapitalize='none'
-                    autoCorrect={false}
-                    style={styles.inputs}/>
-            </View>
-            <View style={{ flexDirection: 'row'}} >
-                <Text style={styles.textInfo}>Password</Text>
-                <TextInput 
-                    placeholder="Password"
-                    returnKeyType="go"
-                    secureTextEntry
-                    style={styles.inputs}/>
-            </View>
-                
-                
+    render() {
+        return (
+        <View style={styles.container}>
+            <Text style={styles.signintext}>SignIn</Text>
+            <KeyboardAvoidingView behavior="padding" enabled>
+                <View style={{ flexDirection: 'row'}} >
+                    <Text style={styles.textInfo}>Email</Text>
+                    <TextInput 
+                        placeholder="Email"
+                        returnKeyType="next"
+                        keyboardType='email-address'
+                        autoCapitalize='none'
+                        onChangeText={(text)=> this.setState({email:text})}
+                        autoCorrect={false}
+                        style={styles.inputs}/>
+                    
+                    {/* {this.getErrorsInField('email').map(errorMessage => <Text>{errorMessage}</Text>) } */}
+                </View>
+                <Text style={styles.errorText}>
+                        {this.state.errorEmail}
+                </Text>
+                <View style={{ flexDirection: 'row'}} >
+                    <Text style={styles.textInfo}>Password</Text>
+                    <TextInput 
+                        placeholder="Password"
+                        returnKeyType="go"
+                        secureTextEntry
+                        onChangeText={(text)=> this.setState({password:text})}
+                        style={styles.inputs}/>
+                </View>
+                <Text style={styles.errorText}>
+                        {this.state.errorPassword}
+                </Text>
+                    
+                    
 
-                <TouchableOpacity onPress={ this.onSubmit} style={styles.button}>
-                    <Text style={styles.buttontext}>SignIn</Text>
-                </TouchableOpacity>
-            </KeyboardAvoidingView>
-      </View>
-    );
-  }
+                    <TouchableOpacity onPress={ this.onSubmit} style={styles.button}>
+                        <Text style={styles.buttontext}>SignIn</Text>
+                    </TouchableOpacity>
+                    
+                </KeyboardAvoidingView>
+        </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        
+        alignItems: 'center',
         justifyContent: 'center',
         flexDirection: 'column',
         
@@ -51,7 +84,7 @@ const styles = StyleSheet.create({
         marginBottom:20,
         backgroundColor: '#95afc0',
         paddingHorizontal: 10,
-        width: 260,
+        width: 230,
         marginRight: -20
     },
     button: {
@@ -71,5 +104,15 @@ const styles = StyleSheet.create({
     textInfo: {
         width: 120,
         textAlign: 'left'
+    },
+
+    errorText: {
+        color: 'red',
+        fontSize: 14,
+        fontWeight: 'bold',
+        marginTop: -10
+    },
+    signintext: {
+        marginBottom: 15
     }
 });
